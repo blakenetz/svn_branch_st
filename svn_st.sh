9e9d82a93b8b92f8ff9c50359f08cb617b46d5fd
Blake Netzeband <blake.netzeband@gmail.com>
2017-01-24 16:48:35 -0700

    uploaded bsh script

---
+++ svn_st.sh
@@ -0,0 +1,20 @@
+st(){
+        dir_color=$'\033[31;1m';
+        color_end=$'\033[0m';
+        for dir in */; do
+                if [[ $dir == app-* ]]; then
+                        cd $dir;
+                        BRANCH=$(svn info | grep 'URL');
+                        if [[ $(svn st) ]]; then
+                                CHANGES=$(svn st);
+                                CHANGEDATE=$(svn info | grep 'Last Changed Date');
+                        else
+                                CHANGES='none';
+                                CHANGEDATE='n/a';
+                        fi;
+
+                        printf "%s%s%s\n Branch %s\n Changes: %s\n %s\n" "$dir_color" "$dir" "$color_end" "$BRANCH" "$CHANGES" "$CHANGEDATE";
+                        cd ..;
+                fi;
+        done;
+}
